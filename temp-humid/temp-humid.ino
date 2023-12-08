88d83109f210a1643c5c1cc7c75c545a07b2618c
Babu_Neil <89362801+Babu-Neil@users.noreply.github.com>
2023-12-08 20:09:51 +0000

    Added Code for LCD

--- temp-humid/temp-humid.ino
+++ temp-humid/temp-humid.ino
@@ -1,14 +1,19 @@
 #include <dht.h> //install the DHTLib library
 dht DHT;
-#define DHT11_PIN 22
+#define DHTPIN 22
 void setup(){
   Serial.begin(9600);
 }
 void loop(){
-  int chk = DHT.read11(DHT11_PIN);
+  int chk = DHT.read11(DHTPIN);
   Serial.print("Temperature = ");
   Serial.println(DHT.temperature);
   Serial.print("Humidity = ");
   Serial.println(DHT.humidity);
-  delay(1000);
+  lcd.setCursor(0,0); 
+  lcd.print("Temp: ");
+  lcd.print(DHT.temperature);
+  lcd.setCursor(0,1);
+  lcd.print("Humidity: ");
+  delay(60000);
 }
