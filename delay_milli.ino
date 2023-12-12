ad343c797a598b06184e6f65bd9404f1ecae9f3a
Babu_Neil <89362801+Babu-Neil@users.noreply.github.com>
2023-12-12 04:32:36 +0000

    Added a delay for lcd

--- delay_milli.ino
+++ delay_milli.ino
@@ -12,3 +12,16 @@ void delay_using_milli(int interval){
   unsigned long time_now = millis();
   while(millis() < time_now + interval){}
 }
+
+#define INTERVAL 60000
+unsigned long currentMillis = millis();
+
+void delay_lcd(int interval, char state){
+  if (state != errorState){
+    if (currentMillis - previousMillis >= interval) {
+      previousMillis = currentMillis;
+
+      displayTempHumidLCD(DHT.temperature, DHT.humidity)
+    }
+  }
+}
